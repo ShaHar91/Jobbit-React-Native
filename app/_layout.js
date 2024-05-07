5412e17e58b1af3a2d9e96a49614f51218294600
ShaHar91 <bollachristiano@gmail.com>
2024-05-07 12:22:43 +0200

    Finishing up the Popular job cards

--- app/_layout.js
+++ app/_layout.js
@@ -1,7 +1,30 @@
 import { Stack } from 'expo-router'
+import { useCallback } from 'react';
+import { useFonts } from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen'
+import AppLoading from 'expo-app-loading';
+
+SplashScreen.preventAutoHideAsync();
 
 const Layout = () => {
-    return <Stack />
+    const [fontsLoaded, fontError] = useFonts({
+        DMBold: require("../assets/fonts/DMSans-Bold.ttf"),
+        DMMedium: require("../assets/fonts/DMSans-Medium.ttf"),
+        DMRegular: require("../assets/fonts/DMSans-Regular.ttf"),
+    });
+
+    const onLayoutRootView = useCallback(async () => {
+        if (fontsLoaded || fontError) {
+            await SplashScreen.hideAsync();
+        }
+    }, [fontsLoaded, fontError]);
+
+    if (!fontsLoaded && !fontError) {
+        console.log("Something still goes wrong...")
+        return <AppLoading />
+    }
+
+    return <Stack onLayoutRootView={onLayoutRootView} />
 }
 
 export default Layout;
